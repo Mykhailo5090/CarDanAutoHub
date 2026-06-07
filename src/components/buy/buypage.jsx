3aadf4a1ffaadddb5b30bf07974a72c5a8c53ae0
Михайло Данканич <mihajlodankanic@MacBook-Air-Mihajlo.local>
2026-06-07 22:50:17 +0300

    buypage done

--- src/components/buy/buypage.jsx
+++ src/components/buy/buypage.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./buypage.scss";
+import yearimg from "../buy/img/year-img.png";
+import engineimg from "../buy/img/engine-img.png";
+// import fuelimg from "../buy/img/fuel-img.png";
+import gearimg from "../buy/img/gear-box-img.png";
+import mileageimg from "../buy/img/mileage-img.png";
+import locationimg from "../buy/img/location-img.png";
 
 const BuyPage = () => {
   const [cars, setCars] = useState([]);
@@ -149,15 +155,15 @@ const BuyPage = () => {
             <div className="filter-box">
               <div className="container_buypage_filtration container_buypage_filtration_1">
                 <p className="p_buypage_filtration">Select Filtration</p>
-                <hr className="hr_select_filtration"/>
+                <hr className="hr_select_filtration" />
               </div>
               <div className="container_hr">
-                  <hr className="hr_filtration" />
-                </div>
+                <hr className="hr_filtration" />
+              </div>
               <div className="container_buypage_filtration container_buypage_filtration_2">
                 <input
                   placeholder="Search (brand, model...)"
-                  className="filter-input"
+                  className="filter-input filter-input_1"
                   onChange={(e) =>
                     setFilters({ ...filters, search: e.target.value })
                   }
@@ -246,21 +252,21 @@ const BuyPage = () => {
                   </div>
                   <div className="container_filter_range container_filter_range_2">
                     <input
-                    type="number"
-                    placeholder="Min:"
-                    className="filter-input"
-                    onChange={(e) =>
-                      setFilters({ ...filters, minPrice: e.target.value })
-                    }
-                  />
-                  <input
-                    type="number"
-                    placeholder="Max:"
-                    className="filter-input"
-                    onChange={(e) =>
-                      setFilters({ ...filters, maxPrice: e.target.value })
-                    }
-                  />
+                      type="number"
+                      placeholder="Min:"
+                      className="filter-input"
+                      onChange={(e) =>
+                        setFilters({ ...filters, minPrice: e.target.value })
+                      }
+                    />
+                    <input
+                      type="number"
+                      placeholder="Max:"
+                      className="filter-input"
+                      onChange={(e) =>
+                        setFilters({ ...filters, maxPrice: e.target.value })
+                      }
+                    />
                   </div>
                 </div>
               </div>
@@ -276,13 +282,13 @@ const BuyPage = () => {
                   </div>
                   <div className="container_filter_range container_filter_range_2">
                     <input
-                  type="number"
-                  placeholder="Max:"
-                  className="filter-input"
-                  onChange={(e) =>
-                    setFilters({ ...filters, maxMileage: e.target.value })
-                  }
-                />
+                      type="number"
+                      placeholder="Max:"
+                      className="filter-input"
+                      onChange={(e) =>
+                        setFilters({ ...filters, maxMileage: e.target.value })
+                      }
+                    />
                   </div>
                 </div>
               </div>
@@ -291,7 +297,7 @@ const BuyPage = () => {
               </div>
             </div>
 
-            {/* ГРІД З ТАЧКАМИ (РЕНДЕРИТЬ ЛИШЕ 10 ШТУК) */}
+           
             <div className="cars-grid">
               {currentCars.map((car) => {
                 const images = JSON.parse(car.images);
@@ -338,36 +344,108 @@ const BuyPage = () => {
 
                     <div className="car-card-info">
                       <div className="car-card-header">
-                        <h3 className="car-card-title">
-                          {car.brand} {car.model}
-                        </h3>
+                        <h3 className="car-card-title">{car.brand}</h3>
+
                         <span className="car-card-price">${car.price}</span>
                       </div>
-                      <p className="car-card-subtext">
-                        {car.year} • {car.fuel_type} • {car.transmission} •{" "}
-                        {car.engine_volume}L
-                      </p>
-                      <p className="car-card-subtext">
-                        📍 {car.region} • 🛣️ {car.mileage} km
-                      </p>
-
-                      {/* --- ДОДАНИЙ БЛОК КОРИСТУВАЧА (ПРОДАВЦЯ) --- */}
-                      <hr className="car-card-divider" />
-                      <div className="car-card-seller">
-                        {car.owner_avatar ? (
-                          <img 
-                            src={`http://localhost:5001${car.owner_avatar}`} 
-                            alt="Seller" 
-                            className="seller-avatar" 
+                      <div className="car-card-header car-car-sub-header">
+                        <h5 className="car-card-title">{car.model}</h5>
+                        <div className="year_container_car_card">
+                          <img
+                            src={yearimg}
+                            alt="year-img"
+                            className="img_car_card img_car_card_1"
                           />
-                        ) : (
-                          <div className="seller-avatar-placeholder">👤</div>
-                        )}
-                        <div className="seller-details">
-                          <p className="seller-name">{car.owner_name || "Продавець"}</p>
-                          <p className="seller-phone">📞 {car.owner_phone || "Не вказано"}</p>
+                          <h5 className="car-card-title">Year: {car.year}</h5>
                         </div>
                       </div>
+
+                      <div className="car-card-container">
+                        <div className="car-card-item_1">
+                          <div className="year_container_car_card year_container_car_card_1">
+                            <img
+                              src={engineimg}
+                              alt="year-img"
+                              className="img_car_card img_car_card_1"
+                            />
+                            <h6 className="car-card-title">
+                               {car.engine_volume}L {car.fuel_type}
+                            </h6>
+                          </div>
+
+                          <div className="year_container_car_card year_container_car_card_1">
+                            <img
+                              src={gearimg}
+                              alt="year-img"
+                              className="img_car_card img_car_card_1"
+                            />
+                            <h6 className="car-card-title">
+                               {car.transmission} 
+                            </h6>
+                          </div>
+
+                          <div className="year_container_car_card year_container_car_card_1">
+                            <img
+                              src={mileageimg}
+                              alt="year-img"
+                              className="img_car_card img_car_card_1"
+                            />
+                            <h6 className="car-card-title">
+                               {car.mileage} km
+                            </h6>
+                          </div>
+                          <div className="year_container_car_card year_container_car_card_1">
+                            <img
+                              src={locationimg}
+                              alt="year-img"
+                              className="img_car_card img_car_card_1"
+                            />
+                            <h6 className="car-card-title">
+                               {car.region}  
+                            </h6>
+                          </div>
+                          <div className="seller-details">
+                            
+                            <h6 className="car-card-title">
+                               📞  {car.owner_phone || "Не вказано"}
+                            </h6>
+                            
+                          </div>
+
+                         
+                          
+                        </div>
+
+                        <div className="car-card-seller">
+                          <div className="car_card_sell_container car_card_sell_container_1">
+                            
+                            {car.owner_avatar ? (
+                            <img
+                              src={`http://localhost:5001${car.owner_avatar}`}
+                              alt="Seller"
+                              className="seller-avatar"
+                            />
+                          ) : (
+                            <div className="seller-avatar-placeholder">👤</div>
+                          )}
+
+                          <p className="car-card-title_22">
+                              {car.owner_name || "Продавець"}
+                            </p>
+
+                          </div>
+                          <div className="car_card_sell_container car_card_sell_container_2">
+
+                          </div>
+                          
+                          
+                          
+                        </div>
+
+
+                      </div>
+
+                      {/* --- ДОДАНИЙ БЛОК КОРИСТУВАЧА (ПРОДАВЦЯ) --- */}
                     </div>
                   </div>
                 );
@@ -376,16 +454,19 @@ const BuyPage = () => {
 
             <div className="container_buypage_filter">
               <select
-                className="filter-input"
+                className="filter_right_sort"
                 onChange={(e) => setSortBy(e.target.value)}
               >
-                <option value="newest">Sort: Newest</option>
-                <option value="cheap">Sort: Cheap</option>
-                <option value="expensive">Sort: Expensive</option>
+                
+                <option value="newest">
+                  Newest
+                  </option>
+                <option value="cheap">Cheap</option>
+                <option value="expensive">Expensive</option>
               </select>
             </div>
           </div>
-          
+
           {/* --- БЛОК ПАГІНАЦІЇ --- */}
           {totalPages > 1 && (
             <div className="pagination">
